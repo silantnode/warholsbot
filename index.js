592aff1f4aec34bfad8befc0b29a7d68ae142877
lenara <lenara@verle.com>
2017-04-16 17:34:02 +0200

    starting of newMarketActivity function

--- index.js
+++ index.js
@@ -201,6 +201,11 @@ bot.on( BALANCE_BUTTON, msg => {
 
 // check if the fountain has been activated and if so display message (compare last activity with last fountain date)
 // check if market has closed (compare last activity with current date, check market closure inbetween)
+ newMarketActivity( msg.from.id, function( error, callback ){
+ console.log(callback);
+});
+
+
 // if market closures happened, see if any bets were won. Display message of sorry or congratulation
 
   GetBalance( msg.from.id, function( error, result ){
@@ -1247,6 +1252,20 @@ function timeConversion(millisec) {
             return days + " days"
         }
     }
+
+// Checks for new Market closures since bets placed
+
+function newMarketActivity( userID, callback ){
+
+    connection.query('SELECT * FROM market_bets WHERE user =' + userID , function( error, result ){
+      
+        if ( error ) return error;
+
+          return callback( error, result );
+
+    });
+
+}
 
       // TO DO:
 
