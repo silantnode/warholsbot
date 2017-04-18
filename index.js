e9930354182d4b06f2ffcccf29ed0bab8b74c98a
ilan katin <silant.mail@gmail.com>
2017-04-18 11:15:39 +0200

    Removed test

--- index.js
+++ index.js
@@ -775,6 +775,7 @@ bot.on( YES_BUTTON, msg => {
 
         if ( error ) throw error;
 
+        console.log(timesViewed[0].viewed);
         console.log(currentGiftSelection[0]);        
 
         let viewedIncrement = ( ( timesViewed[0].viewed ) + 1 );
@@ -1154,7 +1155,7 @@ function GetGiftsContent( callback ){
       // Randomly select 5 items from the table.
       while( currentGiftSelection.length < MAX_LIST_DISPLAY ){
         // Set up the numbers usig minus 1 so that the numbers will read the list properly.
-        let randNum = ( Math.ceil( Math.random() * rows.length -1 ) ); // Temporarily removed -1
+        let randNum = ( Math.ceil( Math.random() * rows.length ) ); // Temporarily removed -1
 
         if( currentGiftSelection.indexOf( randNum ) > -1 ) continue;
 
