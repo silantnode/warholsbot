d64165e4a13c615f43fabb90fbd68c5307e8c1d0
ilan katin <silant.mail@gmail.com>
2017-04-19 17:50:40 +0200

    Commented out

--- index.js
+++ index.js
@@ -739,7 +739,7 @@ bot.on( '/*' , msg => {
 
   }
 
-  
+  /*
   if ( marketFlavor != 0 ) {
 
     console.log('Lets now listen for speculation market activity.');
@@ -747,6 +747,7 @@ bot.on( '/*' , msg => {
     let warholAmount = Number( ( ( msg.text ).slice( 1, 4 ) ) );
 
   }
+  */
 
 });
 
