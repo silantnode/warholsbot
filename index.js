68119879e9d9e28110abe2d71050d0087e0a46f9
ilan katin <silant.mail@gmail.com>
2017-04-20 10:25:27 +0200

    added tests

--- index.js
+++ index.js
@@ -1115,7 +1115,7 @@ function ShareTheWealth( userID, fountainContribution ){
           warholMode = 0;
           giftSpendMode = 0;
 
-          cossole.log('Fountain activated');
+          console.log('Fountain activated');
 
           return bot.sendMessage( userID, `Much generosity activated the Warhols Fountain! Everyone will receive 5 Warhols :D`, { markup });
 
@@ -1123,7 +1123,7 @@ function ShareTheWealth( userID, fountainContribution ){
         
       } else {
 
-        cossole.log('Fountain received new funds');
+        console.log('Fountain received new funds');
 
         return bot.sendMessage( userID, `Thanks for your gift! The Warhols will go to the fountain reservoir and will overflow into everybody’s account soon.`, { markup });
 
